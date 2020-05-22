5670a8863e7fb231776670a9026ce67c4b0e7fa2
Ilya <pbl6a.soad@gmail.com>
2020-05-22 15:07:14 +0300

    add reducers

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import {
   HttpClient
 } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Store, select } from '@ngrx/store';
+import { log } from '../store/actions/users.actions';
 
 @Component({
   selector: 'app-sign-up',
@@ -11,9 +14,14 @@ import {
 export class SignUpComponent implements OnInit {
   login: string;
   password: string;
-  constructor(private http: HttpClient) {}
+  count$: Observable<string>
+  constructor(private store: Store<{ count: string }>, private http: HttpClient) {
+    this.count$ = store.pipe(select('count'));
+  }
   configUrl = 'http://localhost:5000/users/signup';
-
+  log(){
+    this.store.dispatch(log());
+  }
   getJWT() {
     const body = {
       login: this.login,
